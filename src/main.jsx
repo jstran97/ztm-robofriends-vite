bc0e2b7597ae44894943a3ad333a8e2be14b2e40
Justin Tran <justin.s.tran97@gmail.com>
2025-02-28 00:29:16 -0800

    Removed reportWebVitals()

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './containers/App'; // same as App from './App.js';
 // import Card from './Card';
 // same as Card from './Card.js';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -22,4 +22,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
